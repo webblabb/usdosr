358d6a43be23d0c8b329823ee0a9e1a282a7de84
Stefan Sellman <stefan.sellman@liu.se>
2015-02-03 11:28:51 +0100

    Revert "Debugged gridcode"
    This reverts commit a4e0e4505f5a2d0a7a1fba759047fddda2f56bfe.

--- matlab-grid/Matts_gridcode_pwcompare.m
+++ matlab-grid/Matts_gridcode_pwcompare.m
@@ -12,17 +12,17 @@ function [t,S,I] = Program_7_6(N, Size, Y0, RingCull, MaxTime)
 % in a size x size space (in km^2) and Y0 are assumed to be initially infectious.
 % The farm-size, susceptiblity and transmissibility parameters mimic the
 % known UK situation.
-% A ring-cull policy around identified herds can be utilised, RingCull
+% A ring-cull policy around identified herds can be utilised, RingCull 
 % sets the radius of the ring. As an example you may wish to try:
 % Program_7_6(4000,20,5,1,1000);
 %
-% A one-day time step is used together with the short-cuts mentioned in
+% A one-day time step is used together with the short-cuts mentioned in 
 % Box 7.3
 
 % Sets up default parameters if necessary.
 if nargin == 0
     N=4000; % number of farms
-    Size=20;
+    Size=200;
     Y0=1;
     RingCull=0;
     MaxTime=1000;
@@ -43,13 +43,13 @@ CheckGreaterOrEqual(RingCull,0,'Radius of ring cull');
 % Status=12-   => Culled but waiting local ring culling
 % Status=-1    => Culled
 
-x=Size*rand(N,1); y=Size*rand(N,1);
-%x=Size/2*rand(N,1); y=Size/2*rand(N,1); KT
+%x=Size*rand(N,1); y=Size*rand(N,1); 
+x=Size/2*rand(N,1); y=Size/2*rand(N,1); 
 Status=zeros(N,1); Status(1:Y0)=1;
 
 % vectors of x and y coordinates are 4000 random numbers sized up to 20 - maintains a
 % farm/grid cell ratio
-R=rand(N,1);
+R=rand(N,1); 
 Cows=zeros(N,1); % vector of # of cows on each farm
 Sheep=zeros(N,1); % vector of # of sheep on each farm
 n=find(R<0.73); % 73% of farms have cows
@@ -63,25 +63,16 @@ end
 Suscept=Sheep+10.5*Cows; % suscep for each farm based on cows & sheep
 Transmiss=5.1e-7*Sheep + 7.7e-7*Cows; % trans for each farm based on cows & sheep
 
-str = fprintf('Total cows: %.2f; Total sheep: %.2f.\n', sum(Cows), sum(Sheep));
-% disp('Total cows: ')
-% disp(sum(Cows))
-% disp('Total sheep: ')
-% disp(sum(Sheep))
+disp('Total animals: ')
+disp(sum(Cows)+sum(Sheep))
 
-gridsize = 1;
+gridsize = 20;
 grid=WhichGrid(x,y,Size,Size,gridsize,gridsize); % find grid cell for this location
 [tmp,i]=sort(grid);
 x=x(i); y=y(i); Status=Status(i); grid=grid(i);
-seed_loc = i(1); %Save the index of the seeded farm so that it can be reseeded later
-fprintf('Seeding farm: %d.\n', seed_loc);
-fprintf('Number of grid cells: %d.\n', max(grid))
-
 for i=1:max(grid) % for each grid cell
-%     Xgrid(i)=floor((i-1)/gridsize);
-%     Ygrid(i)=mod((i-1),gridsize);
-    Xgrid(i)=(mod((i-1), gridsize) / gridsize) * Size; 
-    Ygrid(i)=(floor((i-1)/gridsize) / gridsize) * Size;
+    Xgrid(i)=floor((i-1)/gridsize);
+    Ygrid(i)=mod((i-1),gridsize);
     m=find(grid==i);
     Num(i)=length(m);
     if Num(i)>0
@@ -94,10 +85,6 @@ for i=1:max(grid) % for each grid cell
         Max_Sus_grid(i)=0;
     end
 end
-scatter(Xgrid, Ygrid, '+r')
-hold on
-scatter(x,y, '.b')
-
 
 %Work out grid to maximum grid transmission probabilities
 for i=1:max(grid)
@@ -112,102 +99,81 @@ for i=1:max(grid)
 end
 
 for r=1:100
-    
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-                            fprintf('\nGridding:')
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    
-    % Susceptible, Exposed, Infectious, Reported.
-    Status=zeros(N,1); Status(seed_loc)=1;
-    t=0; i=1; S=length(find(Status==0)); E=length(find(Status>0 & Status<=5));
-    I=length(find(Status>5 & Status<=9)); R=length(find(Status==10)); R2=length(find(Status>9)); CullSheep=0; CullCattle=0;
-    i=i+1;  IterateFlag=1;
-    
-    
-    % The main iteration
-    all_d_g = [];
-    all_q_g = [];
-    all_d_pw = [];
-    all_q_pw = [];
-    
-    while (t<MaxTime & IterateFlag)
-        
-        [Status, all_d_g, all_q_g]=Iterate(Status, x, y, Suscept, Transmiss, RingCull, grid, first_in_grid, last_in_grid, Num, MaxRate, all_d_g, all_q_g);
-        
-        Sus=find(Status==0); Exp=find(Status>0 & Status<=5); Inf=find(Status>5 & Status<=9);
-        Rep=find(Status>9); Culled=find(Status<0);
-        S(i)=length(Sus); E(i)=length(Exp); I(i)=length(Inf); R(i)=length(find(Status==10)); R2(i)=length(Rep);
-        CullSheep(i)=sum(Sheep(Culled)); CullCattle(i)=sum(Cows(Culled));
-        t(i)=t(i-1)+1; i=i+1;
-        
-        if t(end)>5
-            if (E(end-3)+I(end-3)+R2(end-3))==0
-                IterateFlag=0;
-            end
-        end
-        
-    end
-    
-    %plot(t,I)
-    fprintf('Max N infected at any t: %d.\n', max(I));
-    fprintf('Sheep culled: %.2f; Cattle culled: %.2f.\n', CullSheep(end), CullCattle(end));
-    
-    temp = [max(t) CullSheep(i-1) CullCattle(i-1) max(I)]; % create temp row with time, sheep/cattle culled
-    % write (append) allCulled to text file
-    fID = fopen('matlab_mkgrid.txt','a'); % open file for appending
-    fformat = '%d\t%f\t%f\t%d\n'; % format as fixed decimal, tab-separated, with line break
-    fprintf(fID,fformat,temp); % print to file
-    fclose(fID); % close file
-    
-    %scatter(all_d_g(1:1000), all_q_g(1:1000));
-    
-    
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-                    fprintf('\nPairwise:\n')
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    
-    Status=zeros(N,1); Status(1:Y0)=1;
-    % Susceptible, Exposed, Infectious, Reported.
-    t=0; i=1; S=length(find(Status==0)); E=length(find(Status>0 & Status<=5));
-    I=length(find(Status>5 & Status<=9)); R=length(find(Status==10)); R2=length(find(Status>9)); CullSheep=0; CullCattle=0;
-    i=i+1;  IterateFlag=1;
-    
-    % The main iteration
-    while (t<MaxTime & IterateFlag)
-        
-        [Status, all_d_pw, all_q_pw]=IteratePW(Status, x, y, Suscept, Transmiss, RingCull, all_d_pw, all_q_pw);%, grid, first_in_grid, last_in_grid, Num, MaxRate);
-        
-        Sus=find(Status==0); Exp=find(Status>0 & Status<=5); Inf=find(Status>5 & Status<=9);
-        Rep=find(Status>9); Culled=find(Status<0);
-        S(i)=length(Sus); E(i)=length(Exp); I(i)=length(Inf); R(i)=length(find(Status==10)); R2(i)=length(Rep);
-        CullSheep(i)=sum(Sheep(Culled)); CullCattle(i)=sum(Cows(Culled));
-        t(i)=t(i-1)+1; i=i+1;
-        
-        if t(end)>5
-            if (E(end-3)+I(end-3)+R2(end-3))==0
-                IterateFlag=0;
-            end
-        end
-        
-    end
-    
-    fprintf('Max N infected at any t: %d.\n', max(I));
-    fprintf('Sheep culled: %.2f; Cattle culled: %.2f.\n', CullSheep(end), CullCattle(end));
-    
-    temp = [max(t) CullSheep(end) CullCattle(end) max(I)]; % create temp row with time, sheep/cattle culled
-    % write (append) allCulled to text file
-    fID = fopen('matlab_ktpairwise.txt','a'); % open file for appending
-    fformat = '%d\t%f\t%f\t%d\n'; % format as fixed decimal, tab-separated, with line break
-    fprintf(fID,fformat,temp); % print to file
-    fclose(fID); % close file
+% Susceptible, Exposed, Infectious, Reported.
+t=0; i=1; S=length(find(Status==0)); E=length(find(Status>0 & Status<=5)); 
+I=length(find(Status>5 & Status<=9)); R=length(find(Status==10)); R2=length(find(Status>9)); CullSheep=0; CullCattle=0;
+i=i+1;  IterateFlag=1;
 
-    
+disp('Gridding: ')
+% The main iteration
+while (t<MaxTime & IterateFlag)
+
+    [Status]=Iterate(Status, x, y, Suscept, Transmiss, RingCull, grid, first_in_grid, last_in_grid, Num, MaxRate);
+
+    Sus=find(Status==0); Exp=find(Status>0 & Status<=5); Inf=find(Status>5 & Status<=9); 
+    Rep=find(Status>9); Culled=find(Status<0);
+    S(i)=length(Sus); E(i)=length(Exp); I(i)=length(Inf); R(i)=length(find(Status==10)); R2(i)=length(Rep);
+    CullSheep(i)=sum(Sheep(Culled)); CullCattle(i)=sum(Cows(Culled));
+    t(i)=t(i-1)+1; i=i+1;
+
+    if t(end)>5
+        if (E(end-3)+I(end-3)+R2(end-3))==0
+            IterateFlag=0;
+        end
+    end
     
 end
+    disp('Sheep: ')
+    disp(sum(CullSheep))
+    disp('Cattle: ')
+    disp(sum(CullCattle))
+temp = [max(t) CullSheep(i-1) CullCattle(i-1)]; % create temp row with time, sheep/cattle culled
+% write (append) allCulled to text file
+fID = fopen('matlab_mkgrid.txt','a'); % open file for appending
+fformat = '%d\t%f\t%f\n'; % format as fixed decimal, tab-separated, with line break
+fprintf(fID,fformat,temp); % print to file
+fclose(fID); % close file
+
+disp('Pairwise: ')
+Status=zeros(N,1); Status(1:Y0)=1;
+% Susceptible, Exposed, Infectious, Reported.
+t=0; i=1; S=length(find(Status==0)); E=length(find(Status>0 & Status<=5)); 
+I=length(find(Status>5 & Status<=9)); R=length(find(Status==10)); R2=length(find(Status>9)); CullSheep=0; CullCattle=0;
+i=i+1;  IterateFlag=1;
+
+% The main iteration
+while (t<MaxTime & IterateFlag)
+
+    [Status]=IteratePW(Status, x, y, Suscept, Transmiss, RingCull);%, grid, first_in_grid, last_in_grid, Num, MaxRate);
+
+    Sus=find(Status==0); Exp=find(Status>0 & Status<=5); Inf=find(Status>5 & Status<=9); 
+    Rep=find(Status>9); Culled=find(Status<0);
+    S(i)=length(Sus); E(i)=length(Exp); I(i)=length(Inf); R(i)=length(find(Status==10)); R2(i)=length(Rep);
+    CullSheep(i)=sum(Sheep(Culled)); CullCattle(i)=sum(Cows(Culled));
+    t(i)=t(i-1)+1; i=i+1;
+
+    if t(end)>5
+        if (E(end-3)+I(end-3)+R2(end-3))==0
+            IterateFlag=0;
+        end
+    end
+    
+end
+    disp('Sheep: ')
+    disp(sum(CullSheep))
+    disp('Cattle: ')
+    disp(sum(CullCattle))
+temp = [max(t) CullSheep(i-1) CullCattle(i-1)]; % create temp row with time, sheep/cattle culled
+% write (append) allCulled to text file
+fID = fopen('matlab_ktpairwise.txt','a'); % open file for appending
+fformat = '%d\t%f\t%f\n'; % format as fixed decimal, tab-separated, with line break
+fprintf(fID,fformat,temp); % print to file
+fclose(fID); % close file
+
 end
 
 % Iteration Space
-function [Status, all_d, all_q]=Iterate(Status, x, y, Suscept, Transmiss, RingCull, grid, first_in_grid, last_in_grid, Num, MaxRate, all_d, all_q)
+function [Status]=Iterate(Status, x, y, Suscept, Transmiss, RingCull, grid, first_in_grid, last_in_grid, Num, MaxRate)
 
 Event=0*Status;
 
@@ -218,19 +184,14 @@ for i=1:NI
     INFi=INF(i);
     MaxProb=1-exp(-Transmiss(INFi)*Num.*MaxRate(IGrids(i),:));
     m=find(MaxProb-rand(1,max(grid))>0);  % these are grids that need further consideration
-    
+
     for n=1:length(m)
         s=1;
         M=m(n);
         PAB=1-exp(-Transmiss(INFi)*MaxRate(IGrids(i),M));
         if PAB==1
             ind=first_in_grid(M):last_in_grid(M);
-            sq_dist = (x(INFi)-x(ind)).^2+(y(INFi)-y(ind)).^2;
-            %disp('distance')
-            Q=1-exp(-Transmiss(INFi)*Suscept(ind).*Kernel(sq_dist));
-            all_d = [all_d; sq_dist];
-            all_q = [all_q; Q];
-            %disp([Q, distance, ind'])
+            Q=1-exp(-Transmiss(INFi)*Suscept(ind).*Kernel((x(INFi)-x(ind)).^2+(y(INFi)-y(ind)).^2));
             Event(ind(find(rand(size(Q))-Q<0 & Status(ind)==0)))=1;
         else
             for j=1:Num(M)
@@ -239,11 +200,8 @@ for i=1:NI
                 R=rand(1,1);
                 if R<PAB/P
                     s=0;
-                    sq_dist = (x(INFi)-x(ind)).^2+(y(INFi)-y(ind)).^2;
-                    Q=1-exp(-Transmiss(INFi)*Suscept(ind).*Kernel(sq_dist));
-                    all_d = [all_d; sq_dist];
-                    all_q = [all_q; Q];
-                    if R<Q/P && Status(ind)==0
+                    Q=1-exp(-Transmiss(INFi)*Suscept(ind).*Kernel((x(INFi)-x(ind)).^2+(y(INFi)-y(ind)).^2));
+                    if R<Q/P & Status(ind)==0
                         Event(ind)=1;
                     end
                 end
@@ -262,10 +220,10 @@ for i=1:length(m)
     n=find(D<RingCull.^2);
     Status(n)=-1;
 end
-end
 
 % Iteration Space - pairwise
-function [Status, all_d_pw, all_q_pw]=IteratePW(Status, x, y, Suscept, Transmiss, RingCull, all_d_pw, all_q_pw)%, grid, first_in_grid, last_in_grid, Num, MaxRate)
+function [Status]=IteratePW(Status, x, y, Suscept, Transmiss, RingCull);%, grid, first_in_grid, last_in_grid, Num, MaxRate)
+
 Event=0*Status; % initialize with 0s
 
 INF=find(Status>5 & Status<12);  NI=length(INF); % Note reported farms still infectious
@@ -274,24 +232,20 @@ SUS=find(Status==0); NS=length(SUS); %kt
 
 for i=1:NI
     INFi=INF(i); % for each infectious farm
-    %    MaxProb=1-exp(-Transmiss(INFi)*Num.*MaxRate(IGrids(i),:));
+%    MaxProb=1-exp(-Transmiss(INFi)*Num.*MaxRate(IGrids(i),:));
     % transmissibility of this farm.... max rate for this farm's grid
-    %    m=find(MaxProb-rand(1,max(grid))>0);  % these are grids that need further consideration
+%    m=find(MaxProb-rand(1,max(grid))>0);  % these are grids that need further consideration
     % which grids' probabilities are successful
     for i2=1:NS
-        SUSi=SUS(i2);
-        
+        SUSi=SUS(i);
+    
         R=rand(1,1); %kt
-        sq_dist = (x(INFi)-x(SUSi)).^2+(y(INFi)-y(SUSi)).^2;
-        P=1-exp(-Transmiss(INFi)*Suscept(SUSi).*Kernel(sq_dist)); %kt
-        %fprintf('%.2f, %.2f\n', sq_dist, P)
-        all_q_pw = [all_q_pw; P];
-        all_d_pw = [all_d_pw; sq_dist];
+        P=1-exp(-Transmiss(INFi)*Suscept(SUSi).*Kernel((x(INFi)-x(SUSi)).^2+(y(INFi)-y(SUSi)).^2)); %kt
         if R<P
             Event(SUSi)=1;
         end
     end
-    
+
 end
 m=find(Status>0);
 Status(m)=Status(m)+1; % increase progression for non-susceptibles
@@ -304,7 +258,6 @@ for i=1:length(m)
     n=find(D<RingCull.^2);
     Status(n)=-1;
 end
-end
 
 % Calculates the transmission kernel
 function K=Kernel(dist_squared)
@@ -316,19 +269,16 @@ K=exp(polyval(P,dist_squared));
 K(find(dist_squared<0.0138))=0.3093;
 K(find(dist_squared>60*60))=0;
 
-end
+
 
 %Calculates which grid a particular location is in
-
-function G=WhichGrid(x,y,XRange,YRange,XNum,YNum)
-%input args are: x,y-coordinates of points to check, XY size of whole area,
-%XY size of area covered by grid cells?!?!?
+function G=WhichGrid(x,y,XRange,YRange,XNum,YNum) 
 % XRange and YRange are both 20
-% XNum and YNum are both 20
+% XNum and YNum are both 10
 
 G=floor(x*XNum/XRange)*YNum+floor(y*YNum/YRange)+1;
 % x values * number of farms/10 (grid size)
-end
+
 
 
 % Does a simple check on the value
@@ -337,18 +287,15 @@ m=find(Parameter<Value);
 if ~isempty(m)
     error('Parameter %s(%g) (=%g) is less than %g',str,m(1),Parameter(m(1)),Value);
 end
-end
 
 function []=CheckGreater(Parameter, Value, str)
 m=find(Parameter<=Value);
-if ~isempty(m)>0
+if length(m)>0
     error('Parameter %s(%g) (=%g) is less than or equal to %g',str,m(1),Parameter(m(1)),Value);
-end
 end
 
 function []=CheckLess(Parameter, Value, str)
 m=find(Parameter>=Value);
-if ~isempty(m)
+if length(m)>0
     error('Parameter %s(%g) (=%g) is greater than or equal to %g',str,m(1),Parameter(m(1)),Value);
-end
 end
